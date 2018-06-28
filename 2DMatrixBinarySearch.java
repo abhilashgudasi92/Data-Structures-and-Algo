5553ac368dc23806e9b28e31ded68941bd2402b5
Abhilash Gudasi <abg160130@utdallas.edu>
2018-06-28 16:58:07 -0500

    Create 2DMatrix BinarySearch

---
+++ 2DMatrixBinarySearch.java
@@ -0,0 +1,34 @@
+public static void main(String []args){
+        
+        int mat[][] = { {1,   3,  5,  7},
+                           {10, 11, 16, 20},
+                           {23, 30, 34, 50} };
+            
+        System.out.println(searchMatrix(mat,23));
+     }
+     
+     public static boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int row_num = matrix.length;
+        int col_num = matrix[0].length;
+
+        int begin = 0, end = row_num * col_num - 1;
+
+        while(begin <= end){
+            int mid = (begin + end) / 2;
+            int mid_value = matrix[mid/col_num][mid%col_num];
+
+            if( mid_value == target){
+                return true;
+
+            }else if(mid_value < target){
+                //Should move a bit further, otherwise dead loop.
+                begin = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+
+        return false;
+    }
